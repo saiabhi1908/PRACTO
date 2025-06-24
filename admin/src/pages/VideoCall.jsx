8efb1fb62493203b19687ca559ce2b6a5c55f687
saiabhi1908 <praveenakolli08@gmail.com>
2025-06-24 16:47:37 +0530

    Update VideoCall.jsx

--- admin/src/pages/VideoCall.jsx
+++ admin/src/pages/VideoCall.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { HMSRoomProvider } from "@100mslive/react-sdk";
 
+import VideoConference from "../components/VideoConference";
+
+const VideoCall = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const roomId = searchParams.get("room");
+  const userId = searchParams.get("user");
+  const role = searchParams.get("role");
+
+  return (
+    <HMSRoomProvider>
+      <VideoConference roomId={roomId} userId={userId} role={role} />
+    </HMSRoomProvider>
+  );
+};
+
+export default VideoCall;
